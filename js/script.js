41c0a5dde8c07e66b48e0862419134cec1d9c1b6
git12nazneen <lipinazneen65@gmail.com>
2024-05-31 02:27:14 +0600

    code responsive

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// mix
+
+var mixer = mixitup('.mixit-body');
+
+
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    document.getElementById('nav-list').classList.toggle('show');
+  });
